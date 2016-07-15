920608360a43afc80a7def07daf072312c7ffbae
王然 <wangran354101066@163.com>
2016-07-15 14:10:39 +0800

    Initial Commit

---
+++ TestCutPhoto/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestCutPhoto
+//
+//  Created by 王然 on 16/7/15.
+//  Copyright © 2016年 wangran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
