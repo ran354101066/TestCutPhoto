9e20fdc1f44977e9505540f71ed65ba58a6b25f3
王然 <wangran354101066@163.com>
2016-07-19 14:58:26 +0800

    wr first commit 截图

---
+++ TestCutPhoto/ScreenShotMgr/ScreenShotMgrV.h
@@ -0,0 +1,13 @@
+//
+//  ScreenShotMgrV.h
+//  TestCutPhoto
+//
+//  Created by 王然 on 16/7/15.
+//  Copyright © 2016年 wangran. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScreenShotMgrV : UIView
+
+@end
---
+++ TestCutPhoto/ScreenShotMgr/ScreenShotMgrV.m
@@ -0,0 +1,21 @@
+//
+//  ScreenShotMgrV.m
+//  TestCutPhoto
+//
+//  Created by 王然 on 16/7/15.
+//  Copyright © 2016年 wangran. All rights reserved.
+//
+
+#import "ScreenShotMgrV.h"
+
+@implementation ScreenShotMgrV
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
